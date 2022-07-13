13716d921187df1283e65531305765f4ed119ff8
olaniyitomilola <54294935+olaniyitomilola@users.noreply.github.com>
2022-07-13 21:33:56 +0100

    modular and functional wahala

--- script.js
+++ script.js
@@ -1,30 +1,46 @@
-let btn_login = document.getElementById('btn_login');
 let username = document.getElementById('username');
 let password = document.getElementById('password');
-let errorlogger = document.getElementById('errorlogger');
-errorlogger.style.color = 'red';
-errorlogger.style.fontSize = '0.7em';
-
-btn_login.addEventListener('click', function(e){
-    e.preventDefault;
-    if(username.value.trim() == ''){
-        console.log(username.value)
-        errorlogger.textContent = "*kindly enter your username"
-    } else{
-        if(password.value.trim() === ''){
-            errorlogger.textContent = "*kindly enter your password"
+let errorlogger = document.getElementById('errorlogger');               
+function login (){
+    let btn_login = document.getElementById('btn_login')
+        
+        errorlogger.style.color = 'red';
+        errorlogger.style.fontSize = '0.7em';
+        if(username.value.trim() == ''){
+            errorlogger.textContent = "*kindly enter your username"
         } else{
-            errorlogger.textContent = "login success"
-            errorlogger.style.color = 'green';
+            if(password.value.trim() === ''){
+                errorlogger.textContent = "*kindly enter your password"
+            } else{
+                location.href = 'dashboard.html'
 
-
+            }
         }
-    }
-})
+ 
+}
 
-username.addEventListener('focus',function(e){
+//
+
+function removeError(){
     errorlogger.textContent = '';
-})
-password.addEventListener('focus',function(e){
-    errorlogger.textContent = '';
-})
+}
+
+//navselector
+const actives = document.querySelectorAll('.nav');
+
+
+(function selectActive(){
+
+    actives.forEach((active) => {
+        active.addEventListener('click', () => {
+            removeActiveClasses();
+            active.classList.add('active');
+        });
+    });
+
+    function removeActiveClasses() {
+    actives.forEach((active) => {
+        active.classList.remove('active');
+    });
+    }
+})()
